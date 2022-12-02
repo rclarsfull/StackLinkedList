d1ba5efea71aeaf6e4a5b58c63c4bcb7577b27c5
Lars Przybylek <l.p.1999@live.de>
2022-12-02 13:30:44 +0100

    fertig

--- main.cpp
+++ main.cpp
@@ -3,15 +3,50 @@
 
 using namespace std;
 
+bool testKammern(string in){
+    Stack stack;
+    while(in.length()>0){
+        if(in.at(0)=='('){
+            stack.push('(');
+        }
+        if(in.at(0)=='['){
+            stack.push('[');
+        }
+        if(in.at(0)=='{'){
+            stack.push('{');
+        }
+        if(in.at(0)=='<'){
+            stack.push('{');
+        }
+        if(in.at(0)==')'){
+            if(stack.pop()!='(') return false;
+        }
+        if(in.at(0)==']'){
+            if(stack.pop()!='[') return false;
+        }
+        if(in.at(0)=='}'){
+            if(stack.pop()!='{') return false;
+        }
+        if(in.at(0)=='>'){
+            if(stack.pop()!='<') return false;
+        }
+        in=in.substr(1);
+    }
+    if(stack.size>0) return false;
+    return true;
+}
+
 int main()
 {
-    Stack stack;
-    stack.push('L');
-    stack.push('a');
-    stack.push('r');
-    stack.push('s');
-    cout << stack.size<<endl;
-    cout << stack.pop() << stack.pop() <<stack.pop() <<stack.pop() <<stack.pop() <<endl;
-    cout << stack.size<<endl;
+//    Stack stack;
+//    stack.push('L');
+//    stack.push('a');
+//    stack.push('r');
+//    stack.push('s');
+//    cout << stack.size<<endl;
+//    cout << stack.pop() << stack.pop() <<stack.pop() <<stack.pop() <<stack.pop() <<endl;
+//    cout << stack.size<<endl;
+
+    cout << boolalpha << testKammern("()[[sdfsdg]]");
     return 0;
 }
